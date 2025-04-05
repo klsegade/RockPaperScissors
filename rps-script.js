16877f8324cae1225909f9b2713dfb9c974f05db
klsegade <199424623+klsegade@users.noreply.github.com>
2025-04-04 19:21:22 -0700

    Updated js to add multiple rounds
    Added loop for 5 rounds, return score values,
    and print scores

--- rps-script.js
+++ rps-script.js
@@ -16,40 +16,49 @@ function playRound(humanChoice,computerChoice) {
     if (userInput === 'rock') {
         if (computerChoice === 'paper') {
             console.log('The computer chose paper. Paper beats rock, you lose!')
-            computerScore++
+            let computerScore = 1
+            return [null,computerScore]
         }
         else if (computerChoice === 'scissors') {
             console.log('The computer chose scissors. Rock beats scissors, you win!')
-            humanScore++
+            let humanScore = 1
+            return [humanScore,null]
         }
         else {
             console.log("You both chose rock! It's a tie!")
+            return [null,null]
         }
     }
     else if (userInput === 'paper') {
         if (computerChoice === 'rock') {
             console.log('The computer chose rock. Paper beats rock, you win!')
-            humanScore++
+            let humanScore = 1
+            return [humanScore,null]
         }
         else if (computerChoice === 'scissors') {
             console.log('The computer chose scissors. Scissors beats paper, you lose!')
-            computerScore++
+            let computerScore = 1
+            return [null,computerScore]
         }
         else {
             console.log("You both chose paper! It's a tie!")
+            return [null,null]
         }
     }
     else if (userInput === 'scissors') {
         if (computerChoice === 'paper') {
             console.log('The computer chose paper. Scissors beats paper, you win!')
-            humanScore++
+            let humanScore = 1
+            return [humanScore,null]
         }
         else if (computerChoice === 'rock') {
             console.log('The computer chose rock. Rock beats scissors, you lose!')
-            computerScore++
+            let computerScore = 1
+            return [null,computerScore]
         }
         else {
             console.log("You both chose scissors! It's a tie!")
+            return [null,null]
         }
     }
     else {
@@ -61,10 +70,14 @@ function playGame() {
     humanScore = 0
     computerScore = 0
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    for (let i = 0; i < 3; i++) {     
-    playRound(humanSelection, computerSelection);
+    for (let i = 0; i < 5; i++) {  
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();   
+        let score = playRound(humanSelection, computerSelection);
+        humanScore += score[0]
+        computerScore += score[1]
+        console.log("Your current score is: ", humanScore)
+        console.log("The computer's current score is: ", computerScore)
     }
 }
 
